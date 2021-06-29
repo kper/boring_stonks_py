2470af76ade536f2204d0f0b7fc6a8b1f7941b6a
Kevin Per <kevin.per@protonmail.com>
2021-06-29 11:34:34 +0200

    Refactor module

--- setup.py
+++ setup.py
@@ -1,26 +1,16 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.3' 
 DESCRIPTION = 'API client for boring stonks'
-LONG_DESCRIPTION = ''
 
 # Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
         name="boringstonks", 
         version=VERSION,
         author="Kevin Per",
         author_email="kevin.per@protonmail.com",
         description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=['pandas', 'requests'], # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
-        
+        packages=["boringstonks"],
+        install_requires=['pandas', 'requests'],
         keywords=['python'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Education",
-            "Programming Language :: Python :: 3",
-        ]
 )
