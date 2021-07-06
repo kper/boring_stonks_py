1a723b071a86e9618ebead4fd3f628fd20d90ab8
Kevin Per <kevin.per@protonmail.com>
2021-07-06 07:52:58 +0200

    Working on queries

--- boringstonks/__init__.py
+++ boringstonks/__init__.py
@@ -1,1 +1,5 @@
-from .getStocks import getStocks
+from .api import getStocks
+from .api import getStockById
+from .api import getStockByCIK 
+from .api import getStocksByCIKs 
+from .api import getStocksByTradingSymbol 
--- test.py
+++ test.py
@@ -4,5 +4,13 @@ config = {}
 config['api'] = 'http://localhost:8081/graphql'
 config['timeout'] = 10
 
-print(boringstonks.getStocks("periods { period balanceSheet { assets liabilities  } } ", "access_token", config=config))
+#print(boringstonks.getStocks("periods { period balanceSheet { assets liabilities  } } ", "access_token", config=config))
+
+#print(boringstonks.getStockById(1, "periods { period balanceSheet { assets liabilities  } } ", "access_token", config=config))
+
+#print(boringstonks.getStockByCIK("0001000228", "periods { period balanceSheet { assets liabilities  } } ", "access_token", config=config))
+
+#print(boringstonks.getStocksByCIKs(["0001000228"], "periods { period balanceSheet { assets liabilities  } } ", "access_token", config=config))
+
+print(boringstonks.getStocksByTradingSymbol(["B"], "periods { period balanceSheet { assets liabilities  } } ", "access_token", config=config))
 
