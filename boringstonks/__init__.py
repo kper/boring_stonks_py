83512e26abdb18413b167c181760b5d911d322ba
Kevin Per <kevin.per@protonmail.com>
2021-07-30 07:34:08 +0200

    Add prelude export

--- boringstonks/__init__.py
+++ boringstonks/__init__.py
@@ -3,3 +3,5 @@ from .api import getStockById
 from .api import getStockByCIK 
 from .api import getStocksByCIKs 
 from .api import getStocksByTradingSymbol 
+from .api import getLastOhlcBySymbol 
+from .api import getLastOhlcByCIK
