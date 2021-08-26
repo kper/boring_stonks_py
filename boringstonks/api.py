b17e80393844e8083d95fda67c82e42cd7153c15
Kevin Per <kevin.per@protonmail.com>
2021-08-26 07:39:06 +0200

    Dropping keys which are always Nan

--- boringstonks/api.py
+++ boringstonks/api.py
@@ -9,6 +9,8 @@ defaultConfig['api'] = 'https://boringstonks.com/graphql'
 defaultConfig['timeout'] = 50
 
 def handle(config, access_token, mod_query, key):
+    config = config.copy()
+    config.update(defaultConfig)
     headers = {'Content-Type': 'application/json', 'Access-Token': access_token}
     x = requests.post(config['api'], data=mod_query, timeout=config['timeout'], headers=headers)
     if x.status_code == 200:
@@ -28,7 +30,10 @@ def handle(config, access_token, mod_query, key):
         return
 
 
-def handle_req(config, access_token, mod_query, key):
+def handle_req(config, access_token, mod_query, key, periods):
+    config = config.copy()
+    config.update(defaultConfig)
+
     headers = {'Content-Type': 'application/json', 'Access-Token': access_token}
     x = requests.post(config['api'], data=mod_query, timeout=config['timeout'], headers=headers)
     if x.status_code == 200:
@@ -41,6 +46,22 @@ def handle_req(config, access_token, mod_query, key):
         if stocks is not None:
             fl = [flatten(d) for d in stocks]
             frame = pandas.DataFrame(fl) 
+
+            # There are some columns which are always Nan
+            # because they are wrapper keys
+
+            columns = ["balanceSheet", "incomeStatement", "cashflowStatement"]
+            columns_to_delete = []
+            for i in range(0, periods):
+                for j in columns:
+                    columns_to_delete.append(f'periods_{i}_{j}')
+
+            # ohlc is not index by period
+            columns_to_delete.append('ohlc')
+
+            # Drop columns
+            frame.drop(columns_to_delete, axis=1, inplace=True, errors='ignore')
+
             return frame
         else:
             return pandas.DataFrame() 
@@ -51,26 +72,26 @@ def handle_req(config, access_token, mod_query, key):
 
 def getStocks(query, access_token, periods = 0, config = defaultConfig):
     mod_query = '{"query": "{ getStocks(periods: %d) { %s } } "}' % (periods, query)
-    return handle_req(config, access_token, mod_query, 'getStocks') 
+    return handle_req(config, access_token, mod_query, 'getStocks', periods) 
 
 def getStockById(iid, query, access_token, periods = 0, config = defaultConfig):
     mod_query = '{"query": "{ getStockById(id: %d, periods: %d) { %s } } "}' % (iid, periods, query)
-    return handle_req(config, access_token, mod_query, 'getStockById') 
+    return handle_req(config, access_token, mod_query, 'getStockById', periods) 
 
 def getStockByCIK(cik, query, access_token, periods = 0, config = defaultConfig):
     mod_query = '{"query": "{ getStockByCIK(cik: %s, periods: %d) { %s } } "}' % ("\\\"" + cik + "\\\"", periods, query)
-    return handle_req(config, access_token, mod_query, 'getStockByCIK') 
+    return handle_req(config, access_token, mod_query, 'getStockByCIK', periods) 
 
 def getStocksByCIKs(ciks, query, access_token, periods = 0, config = defaultConfig):
     cik_str = ','.join(["\\\"" + str(elem) + "\\\"" for elem in ciks])
     mod_query = '{"query": "{ getStocksByCIKs(ciks: [%s], periods: %d) { %s } } "}' % (cik_str, periods, query)
-    return handle_req(config, access_token, mod_query, 'getStocksByCIKs') 
+    return handle_req(config, access_token, mod_query, 'getStocksByCIKs', periods) 
 
 
 def getStocksByTradingSymbol(syms, query, access_token, periods = 0, config = defaultConfig):
     sym_str = ','.join(["\\\"" + str(elem) + "\\\"" for elem in syms])
     mod_query = '{"query": "{ getStocksByTradingSymbol(sym: [%s], periods: %d) { %s } } "}' % (sym_str, periods, query)
-    return handle_req(config, access_token, mod_query, 'getStocksByTradingSymbol') 
+    return handle_req(config, access_token, mod_query, 'getStocksByTradingSymbol', periods) 
 
 def getLastOhlcBySymbol(sym, query, access_token, config = defaultConfig):
     mod_query = '{"query": "{ getLastOhlcBySymbol(sym: %s) { %s } } "}' % ("\\\"" + sym + "\\\"", query)
---
+++ main.py
@@ -0,0 +1,6 @@
+import boringstonks
+
+data = boringstonks.getLastOhlcBySymbol("VEND", "fromDate open close", "access_token")
+data2 = boringstonks.getStocks("entityCentralIndexKey entityRegistrantName periods { balanceSheet { cashAndCashEquivalentsAtCarryingValue marketableSecurities receivablesNetCurrent stockHoldersEquity assetsCurrent liabilitiesCurrent assets goodwill liabilities entityCommonStockSharesOutstanding commonStockSharesOutstanding  } incomeStatement { revenueFromContractWithCustomerIncludingAssessedTax operatingIncomeLoss netIncomeLoss } } ohlc { close }", "access_token", periods=1)
+print(data)
+print(data2)
